35bc1bd80a9f03da72737f01815fc829dc3c3fdd
naroaazcona <naroa.azcona@opendeusto.es>
2025-03-03 10:49:45 +0100

    Cambio main

--- main.c
+++ main.c
@@ -3,16 +3,46 @@
 #include "domain/menu.h"
 
 int main(){
-	char opcion, opcionInicioAdmin;
+	char opcion, opcionClienteInicio, opcionCliente,opcionAdminInicio;
 
 	do{
 		opcion = menuPrincipal();
 		switch(opcion){
 			case '1':
-				menuAdministradorInicio();
-				break;
+				do{
+					opcionAdminInicio = menuAdministradorInicio();
+					switch(opcionAdminInicio){
+					case '1':
+
+						break;
+					case '0': printf("Volviendo al menú principal...\n");
+						      fflush(stdout);
+						      break;
+					default: printf("Error! La opción seleccionada no es correcta\n");
+						     fflush(stdout);
+					}
+				}while(opcionAdminInicio != '0');
+					break;
 			case '2':
-				menuClienteInicio();
+				do{
+					opcionClienteInicio = menuClienteInicio();
+					switch(opcionClienteInicio){
+						case '1':
+							//iniciarSesionCliente();
+							break;
+						case '2':
+							//registroCliente();
+							break;
+						case '0':
+							printf("Hasta pronto!!\n");
+							break;
+						default:
+							printf("Opción invalida. Por favor, ingrese una opción válida.\n");
+							fflush(stdout);
+					}
+
+				}while(opcion !='0');
+
 				break;
 			case '0':
 				printf("Hasta pronto!!\n");
@@ -27,6 +57,9 @@ int main(){
 	return 0;
 
 }
+
+
+/**/
 
 
 
